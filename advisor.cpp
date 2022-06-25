86a03b73db60e6369d1bf9ee1afdf4d4cdbb4cc2
LAW <lawrencelkim@gmail.com>
2022-06-25 08:22:51 -0400

    still need best word suggesting algorithm

---
+++ advisor.cpp
@@ -0,0 +1,13 @@
+#include "solver.h"
+
+using namespace std;
+
+int main(){
+
+    thelistmaker(); //make thelist of all words in "words.txt" | contains 12972 of 5 letter words
+    
+    cout << run();
+
+    
+    return 0;
+}
--- solver.h
+++ solver.h
@@ -5,6 +5,8 @@
 #include <array>
 #include <fstream>
 #include <sstream>
+#include <map>
+#include <unordered_map>
 
 using namespace std;
 
@@ -24,12 +26,22 @@ struct Word{
 };//END word struct
 
 //THINGS TO KEEP TRACK TO GET SOLTION
+const string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+map<char, map<int, vector<int>>> master;
+//this is mapped to every letter. for every letter, records index in which is shows up in word
+//and records word in index form within thelist
+
 vector<Word> thelist, guesses;
 vector<char> valid, invalid;
+vector<int> combine;
+
+string guessbool = "";
 
 
 //FUNCTIONS
 void thelistmaker(){
+    cout << "MAKING THE AND MASTER LIST..." << endl << endl;
     //since there are a lot of words, should reserve the amount of space that we will use.
     thelist.reserve(12972);
     
@@ -39,7 +51,29 @@ void thelistmaker(){
 
     //add contents to thelist
     string tempword;
-    while (getline(thewords, tempword)) thelist.push_back(tempword);
+    uint32_t counter = 0;
+    while (getline(thewords, tempword)){
+        thelist.push_back(tempword);
+        for (int i = 0; i < 5; i++){
+            master[tempword[i]][i].push_back(counter);
+        }counter++;
+    }
+
+    // cout << "THIS IS MASTER SIZE: " << master.size() << endl;
+    // cout << "THIS IS 'A' SIZE: " << master['a'].size() << endl;
+    // cout << "THIS IS HOW MANY WORDS HAVE A IN FIRST POSITION: " << master['a'][0].size() << endl;
+    // cout << "THIS IS THE FIRST WORD WITH A IN THE BEGINNING: " << thelist[master['a'][0][0]].word << endl;
+    // cout << "THIS IS VECTOR[4]: " << thelist[4].word << endl;
+
+
+
+    //these words are found online
+    cout << "THESE ARE RECOMMENDED START WORDS:\n" <<
+        "SOARE | ROATE | RAISE | CRANE | SLATE\n" <<
+        "SEARE | STARE | SLANT | TRACE | ROAST\n" <<
+        "ADIEU | SNARE | RAISE | AESIR | MONKEYU (jk)\n" <<
+        "but of course, you are free to choose whatever you like !\n\n"; 
+
 }//END thelistmaker()
 
 void checkguess(Word &word){
@@ -48,20 +82,25 @@ void checkguess(Word &word){
     // 1 = yellow
     // 2 = green
 
-    string guessbool;
     cout << "0: wrong | 1:wrong place | 2:correct: ";
     getline(cin >> ws, guessbool);
 
     vector<char> yellow; yellow.reserve(5);
 
     for (uint32_t i = 0; i < 5; i++){
+        vector<int> combiner, sfinal;
         if (guessbool[i] == '2'){
             word.word[i] = toupper(word.word[i]);
+
+            //find all indices that have char at i and add into combine... EDIT: THIS METHOD IS TOO LONG AND CONSUMING
+            //find another one
         }
         else if(guessbool[i] == '1'){
             yellow.push_back(word.word[i]);
         }
         else if ((guessbool[i]) > '2' || (guessbool[i]) < '0') throw runtime_error("Invalid character bool input");
+
+
     }
 
     //THIS PART DOES NOT NEED TO BE INCLUDED. ONLY FOR VISUALIZATION
@@ -75,7 +114,7 @@ void checkguess(Word &word){
 
 string run(){
     uint32_t attempts = 0;
-    while (attempts != 6){
+    while (attempts != 6 && guessbool != "22222"){
         string in;
         cout << "YOUR GUESS ('q' to quit): ";
         getline(cin >> ws, in);
