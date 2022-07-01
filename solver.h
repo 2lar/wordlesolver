38daa681bb1f496f9734372618b13c2b14d4f4b0
LAW <lawrencelkim@gmail.com>
2022-07-01 19:42:43 -0400

    basic complete (?)

--- solver.h
+++ solver.h
@@ -1,18 +1,16 @@
 #include "solver.hpp"
 
-
 using namespace std;
 
 unordered_map<char, unordered_map<int, unordered_set<int>>> master;
 
 vector<Word> thelist, guesses;
-vector<int> initial =  {0,1,2,3,4}; int ideleted = 0;
+vector<int> initial = {0,1,2,3,4}; int ideleted = 0;
 
-unordered_map<char, int> partialinvalid; //pair between the char and pos that is invalid;
-unordered_map<char, int> totinvalid, frequencies;
-unordered_set<int> resultant, invalidlist, yellowed, thefinal;
+unordered_map<char, int> frequencies;
+unordered_set<int> resultant, invalidlist, thefinal;
 unordered_map<int, char> valid;
-unordered_set<char> invalidchar, validchar;
+unordered_set<char> invalidchar, validchar, yellowed;
 
 map<char, vector<int>> freqatpos;
 
@@ -34,7 +32,6 @@ void thelistmaker(){
         frequencies[c] = 0;
     }//END for loop
 
-
     thelist.reserve(12973);
 
     ifstream thewords;
@@ -54,72 +51,42 @@ void thelistmaker(){
     }
     firstsuggestions();
 
-    // for (auto &c : alphabet){
-    //     cout << c << ": " << frequencies[c] << endl;
-    // }
 }//END listmaker
 
 unordered_set<int> intersections(const list<unordered_set<int>> &ilist){
     unordered_map<int, int> checker;
     unordered_set<int> ireturn;
-    const int isize = valid.size();
-    // for (auto &v : valid){
-    //     cout << v.first << ' ';
-    // }cout << endl;
+    const int isize = ilist.size();
 
     for (const auto &sections : ilist){
         for (const auto &wordidx : sections){
             checker[wordidx] += 1;
-            // cout << thelist[wordidx].word << endl;
         }
     }
-
     for (const auto &e : checker){
         if (checker[e.first] == isize) ireturn.insert(e.first);
     }
-
-    // cout << "THIS IS IRETURN" << endl;
-    // for (auto &i : ireturn){
-    //     cout << thelist[i].word << endl;
-    // }
 
     return ireturn;
 }//END intersections()
 
 void onlyvalids(unordered_set<int> &thefinal, unordered_set<int> &invalidlist){
     if (invalidlist.size() == 0){
-        // cout << "returning from onlyvalids" << endl;
         return;
     }
     for (auto &key : thefinal){
         unordered_set<int>::iterator ovc = invalidlist.find(key);
         if (ovc != invalidlist.end()){
-            // cout << "this is ovc: " << thelist[*ovc].word << endl;
             thefinal.erase(key);
         }
 
-        // cout << "THIS IS INVALID CHAR LIST: ";
-        // for (auto &c : invalidchar){
-        //     cout << c << " ";
-        // }cout << endl;
-
-        // cout << "THIS IS THE WORD: " << thelist[key].word << endl;
-        // cout << "THIS IS THE char in the string: ";
         for (auto &s : thelist[key].word){
-            // cout << s << " ";
             unordered_set<char>::iterator invc = invalidchar.find(s);
             if (invc != invalidchar.end()){
-                // cout << "DELETING BECAUSE IT FOUND INVALID CHAR" << endl;
                 thefinal.erase(key);
             }
         }
-        // cout << endl;
     }
-
-    // cout << "THEFINAL BEFORE SUGGEST IN ONLYVALID" << endl;
-    // for (auto &w : thefinal){
-    //     cout << thelist[w].word << endl;
-    // }cout << endl;
 }//END onlyvalids
 
 void checkguess(Word &word, int attempts){
@@ -130,23 +97,12 @@ void checkguess(Word &word, int attempts){
     unordered_set<int> yellowset;
     list<unordered_set<int>> tointersect;
     
-
-    // cout << "THIS IS VALID SIZE: ";
-    // cout << valid.size() << endl;
-
     for (int i = 0; i < 5; i++){
         if (guessbool[i] == '2'){
             valid[i] = word.word[i];
             validchar.insert(word.word[i]);
-            // cout << "UPDATED VALID SIZE: ";
-            // cout << valid.size() << endl;
-            //remove pos i from initial and insert vector of words from char at pos i into intersector
-            // for (auto &u : master[word.word[i]][i]){
-            //     cout << thelist[u].word << endl;
-            // }
             tointersect.push_back(master[word.word[i]][i]);
             
-
             //delete green idx from initial
             vector<int>::iterator initit = find(initial.begin(), initial.end(), i);
             if (initit != initial.end()) initial.erase(initial.begin() + i - ideleted++);
@@ -156,18 +112,16 @@ void checkguess(Word &word, int attempts){
 
         else if (guessbool[i] == '1'){
             yellow.emplace_back(word.word[i], i);
+            yellowed.insert(word.word[i]);
         }
 
         else if (guessbool[i] == '0'){
             //adding EACH word into the invalid list
             for (auto &u : master[word.word[i]][i]){
-                // cout << "INSERT INTO INVALIDLIST: " << thelist[u].word << endl;
                 invalidlist.insert(u);
             }
             if (validchar.find(word.word[i]) == validchar.end()){
-                // cout << "INSERT INTO INVALIDCHAR: " << word.word[i] << endl;
                 if (word.word.substr(i+1).find(word.word[i]) == string::npos){
-                    // cout << "adding into invalid char" << endl;
                     invalidchar.insert(word.word[i]);
                 }
             }
@@ -175,9 +129,6 @@ void checkguess(Word &word, int attempts){
 
         else throw runtime_error("==== Invalid character bool input ====");
     }//end for loop
-
-    // cout << "THIS IS VALID SIZE:";
-    // cout << valid.size() << endl;
 
     //ADD INTO FREQUENCY
     for (auto &c : word.word){
@@ -195,61 +146,27 @@ void checkguess(Word &word, int attempts){
         }
     }//END for loop
 
-    // cout << "THIS IS TO INTERESECT" << endl;
-    // for (auto &c : tointersect){
-    //     for (auto &k : c){
-    //         cout << thelist[k].word << endl;
-    //     }
-    // }cout << endl;
-
-    // cout << "THIS IS YELLOWSET" << endl;
-    // for (auto k : yellowset){
-    //     cout << thelist[k].word << endl;
-    // }cout << endl;
-
     //post finding intersections
-    // cout << "THIS IS RESULTANT" << endl;
     if (initial.size() < 5){
         resultant = intersections(tointersect);
-
-        // cout << "SEEING: ";
-        // for (auto &p : resultant){
-        //     cout << thelist[p].word << ' ';
-        // }cout << endl;
-
         
         if (yellowset.size() != 0){
-            // cout << "YELLOWSET SIZE is not 0: " << yellowset.size() << endl;
             thefinal = intersections({resultant, yellowset});
             resultant = thefinal;
         }
 
-
-        
         onlyvalids(resultant, invalidlist);
         suggest(resultant, deleted, thelist, frequencies);
-
-
-
-        // for (auto &s : scores){
-        //     cout << s.first << ": " << s.second << endl;
-        // }
-
-        
-
-        //need to make the suggestion based on strongest frequency score.
-        
+      
     }
     else{
         //use the frequenct list to compute the next best suggestion based on 5 invalids.
         cout << "you got nothing remotely close in the first try" << endl;
     }
-    // cout << "end of checkguess" << endl;
 }//END checkguess
 
 void run(){
     // test();
-
     uint32_t attempts = 0;
     while (attempts != 6 && guessbool != "22222"){
         string in;
@@ -265,27 +182,13 @@ void run(){
             //NEED CHECK WORD TO ANSWER // CHECK CORRECTNESS - use helper function
             checkguess(guess, attempts);
 
-            // cout << "pushing into guesses" << endl;
             guesses.push_back(guess);
-
-            // cout << "THIS IS invalidlist: ";
-            // for (auto &i : invalidlist){
-            //     cout << thelist[i].word << " ";
-            // }
-            // cout << endl << endl;
-
-            // cout << "THIS IS totinvalid: ";
-            // for (auto &i : totinvalid){
-            //     cout << '(' << i.first << ", " << i.second << ')';
-            // }
-            // cout << endl << endl;
 
             attempts++;
         }catch (runtime_error &e){
             cerr << e.what() << endl;
         }
 
-        // cout << "MADE IT TO SHOWGUESSES" << endl;
         showguesses(guesses);
 
         cout << "YOUR ATTEMPTS: " << attempts << endl << endl;
